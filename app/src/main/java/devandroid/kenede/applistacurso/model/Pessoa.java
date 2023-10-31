7ebd38ce385fb040b0aa7140e0308f7a5d3e1b3a
kenede de lima pires <55933535+kenedelima@users.noreply.github.com>
2023-10-31 10:39:02 -0300

    Criação Classe Pessoa camada MODEL

---
+++ app/src/main/java/devandroid/kenede/applistacurso/model/Pessoa.java
@@ -0,0 +1,14 @@
+package devandroid.kenede.applistacurso.model;
+// criando a classe pessoa
+public class Pessoa {
+
+    //Construtor
+    //Usar o public para poder criar o objeto
+
+    public Pessoa(){
+
+
+    }
+
+
+}
--- app/src/main/java/devandroid/kenede/applistacurso/view/MainActivity.java
+++ app/src/main/java/devandroid/kenede/applistacurso/view/MainActivity.java
@@ -1,14 +1,23 @@
-package devandroid.kenede.applistacurso;
+package devandroid.kenede.applistacurso.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import devandroid.kenede.applistacurso.R;
+import devandroid.kenede.applistacurso.model.Pessoa;
+
 public class MainActivity extends AppCompatActivity {
 
+    Pessoa pessoa; // Declaração objeto pessoa
+
+
+    // Metodo
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        pessoa = new Pessoa(); // Cria o objeto pessoa
     }
 }
